d9d8845d8b2ac6321c929c610855b34a4fd8dfa2
freeitgroupe <freeitgroupe@gmail.com>
2021-07-19 15:20:16 +0300

    19072021

--- introspection.js
+++ introspection.js
@@ -5,18 +5,29 @@ const a = Math.sin;
 const b = [30,45,60].map(a)
 console.log(b);
 */
-//атрибуты ф-ии
+
+// атрибуты ф-ии
+// function declaration (будет виден во всех частях файла)
 function inc(a) {
   return ++a;
 }
 
+// function expression будет виден после того как мы его присвоили в переменную
+// зн-е функционального типа присвоили в идентификатор sum 
 const sum = function(a, b) {
   return a + b;
 };
 
+// лямда экспрешен или ф-я стрелка
+// (a > b ? a : b) - возвращается как return этой ф-ии
+// 
 const max = (a, b) => (a > b ? a : b);
 
-
+// лямда ф-я с блоком операторов { } в таком случае пишем return в конце. 
+const avg = (a, b) => { 
+	const s = sum(a,b);
+	return s/2;
+}
 
 console.log('Names: ');
 console.dir({
